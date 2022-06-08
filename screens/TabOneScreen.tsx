816ad625cf71e34e33a9e7ea9cb06d66bb8c3f8d
Milo de Vries <milo@xdh.nl>
2022-06-08 16:45:36 +0200

    WIP license plate recognition

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,17 +1,30 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity } from 'react-native';
 
+import Colors from '../constants/Colors';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
-import { ApiClient } from 'cloudmersive-image-api-client'
+import { ApiClient, RecognizeApi } from 'cloudmersive-image-api-client'
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
+    <View>
     <View style={styles.container}>
-      <Text style={styles.title}>GuzzlerDetector</Text>
+    <TouchableOpacity onPress={lookupLicencePlate} style={styles.helpLink}>
+      <Text style={styles.title}>GuzzlerDetectorKnop</Text>
+      </TouchableOpacity>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+    </View>
+
+    {/* <View style={styles.helpContainer}>
+        <TouchableOpacity onPress={lookupLicencePlate} style={styles.helpLink}>
+          <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
+            Tap here if your app doesn't automatically update after making changes
+          </Text>
+        </TouchableOpacity>
+    </View> */}
     </View>
   );
 }
@@ -31,4 +44,37 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  helpContainer: {
+    marginTop: 15,
+    marginHorizontal: 20,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    textAlign: 'center',
+  },
 });
+
+function lookupLicencePlate() {
+  const defaultClient = ApiClient.instance
+  const Apikey = defaultClient.authentications['Apikey']
+
+  Apikey.apiKey = '944b5a14-1007-45b4-996b-f8f89545c381';
+ 
+  let apiInstance = new RecognizeApi();
+
+  let imageFile = "../pink_pickup.jpg"
+
+  let callback = function(error, data, response) {
+    if (error) {
+      return(error)
+    } else {
+      return(
+        'API called successfully. Returned data: ' + data)
+      }}
+  
+      apiInstance.recognizeDetectVehicleLicensePlates(imageFile, callback)
+
+}
